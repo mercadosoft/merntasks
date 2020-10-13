585fbc144aba38ec247073e391bbbe142f8b83e1
Esteban Burgos <estebanburgos85@gmail.com>
2020-10-13 16:55:55 -0300

    add all projects from database

--- cliente/src/components/proyectos/ListadoProyectos.js
+++ cliente/src/components/proyectos/ListadoProyectos.js
@@ -1,29 +1,40 @@
 import React, { useContext, useEffect } from 'react';
 import Proyecto from './Proyecto';
 import proyectoContext from '../../context/proyectos/proyectoContext';
+import AlertaContext from '../../context/alertas/alertaContext';
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const ListadoProyectos = () => {
 
     // Obtener el state de proyectos importando el Context donde se encuentra el state inicial
-    const proyectosContext = useContext(proyectoContext);
     // Extraer proyectos del state inicial
-    const { proyectos, obtenerProyectos } = proyectosContext;
+    const proyectosContext = useContext(proyectoContext);
+    const { mensaje, proyectos, obtenerProyectos } = proyectosContext;
+
+    // Extraer context de alertas
+    const alertaContext = useContext(AlertaContext);
+    const { alerta, mostrarAlerta } = alertaContext;
 
     useEffect(() => {
+
+        // Si existe un error
+        if(mensaje){
+          mostrarAlerta(mensaje.msg, mensaje.categoria);
+        }
         obtenerProyectos();
         // eslint-disable-next-line
-    }, []);
+    }, [mensaje]);
 
     // Validar si no existen proyectos creados
     if ( proyectos.length === 0 ) return <h3 className="mensaje">No hay proyectos comienza creando uno.</h3>;
 
     return (
       <ul className="listado-proyectos">
+        { alerta ? ( <div className={`alerta ${alerta.categoria}`}>{alerta.msg}</div> ) : null }
         <TransitionGroup>
           {proyectos.map((proyecto) => (
             <CSSTransition
-                key={proyecto.id}
+                key={proyecto._id}
                 timeout={500}
                 classNames="proyecto"
             >
--- cliente/src/context/proyectos/proyectoState.js
+++ cliente/src/context/proyectos/proyectoState.js
@@ -1,5 +1,4 @@
 import React, { useReducer } from 'react';
-import { v4 as uuid } from "uuid";
 
 import proyectoContext from './proyectoContext';
 import proyectoReducer from './proyectoReducer';
@@ -9,22 +8,20 @@ import {
     AGREGAR_PROYECTO,
     VALIDAR_FORMULARIO,
     PROYECTO_ACTUAL,
-    ELIMINAR_PROYECTO
+    ELIMINAR_PROYECTO,
+    PROYECTO_ERROR
  } from '../../types'
 
-const ProyectoState = props => {
+ import clienteAxios from '../../config/axios';
 
-    const proyectos = [
-        { id: 1, nombre: 'Tienda Virtual' },
-        { id: 2, nombre: 'Blog' },
-        { id: 3, nombre: 'Website' }
-    ]
+const ProyectoState = props => {
 
     const initialState = {
         proyectos : [],
         formulario : false,
         errorformulario : false,
-        proyecto: null
+        proyecto: null,
+        mensaje: null
     }
 
     // Dispatch para ejecutar las acciones 
@@ -38,24 +35,49 @@ const ProyectoState = props => {
     }
 
     // Obtener los proyectos 
-    const obtenerProyectos = () => {
-        dispatch({
-            type : OBTENER_PROYECTOS,
-            payload : proyectos
-        })
+    const obtenerProyectos = async () => {
+        try {
+            const resultado = await clienteAxios.get('api/proyectos');
+
+            dispatch({
+              type: OBTENER_PROYECTOS,
+              payload: resultado.data.proyectos
+            });
+
+        } catch (error) {
+            const alerta = {
+                msg: 'Hubo un error',
+                categoria: 'alerta-error'
+            }
+            dispatch({
+                type: PROYECTO_ERROR,
+                payload: alerta
+            })
+        }
     }
 
     // Agregar nuevo proyecto 
-    const agregarProyecto = proyecto => {
+    const agregarProyecto = async (proyecto) => {
         
-        // Asignar un id al proyecto en caso que no sea un string vacio o exista
-        proyecto.id = uuid();
+        try {
+            const resultado = await clienteAxios.post('/api/proyectos', proyecto);
+            console.log(resultado);
+            // Insertar el proyecto en el state
+            dispatch({
+                type : AGREGAR_PROYECTO,
+                payload : resultado.data
+            })
 
-        // Insertar el proyecto en el state
-        dispatch({
-            type : AGREGAR_PROYECTO,
-            payload : proyecto
-        })
+        } catch (error) {
+            const alerta = {
+                msg: 'Hubo un error',
+                categoria: 'alerta-error'
+            }
+            dispatch({
+                type: PROYECTO_ERROR,
+                payload: alerta
+            })
+        }
     }
 
     // Validar el formulario por errores
@@ -74,11 +96,23 @@ const ProyectoState = props => {
     }
 
     // Eliminar un proyecto
-    const eliminarProyecto = proyectoId => {
-        dispatch({
-            type: ELIMINAR_PROYECTO,
-            payload: proyectoId
-        })
+    const eliminarProyecto = async (proyectoId) => {
+        try {
+            await clienteAxios.delete(`/api/proyectos/${proyectoId}`);
+            dispatch({
+              type: ELIMINAR_PROYECTO,
+              payload: proyectoId,
+            });
+        } catch (error) {
+            const alerta = {
+                msg: 'Hubo un error',
+                categoria: 'alerta-error'
+            }
+            dispatch({
+                type: PROYECTO_ERROR,
+                payload: alerta
+            })
+        }
     }
 
 
@@ -89,6 +123,7 @@ const ProyectoState = props => {
           formulario: state.formulario,
           errorformulario: state.errorformulario,
           proyecto: state.proyecto,
+          mensaje: state.mensaje,
           mostrarFormulario,
           obtenerProyectos,
           agregarProyecto,
